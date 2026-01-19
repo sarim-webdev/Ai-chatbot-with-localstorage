154f90d388fbd072872496f83bd641152d1926db
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-01-19 12:35:35 +0500

    react

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import Sidebar from "../components/layout/Sidebar";
 import ChatWindow from "../components/chatbot/ChatWindow";
 import ChatInput from "../components/chatbot/ChatInput";
+import { FaBars } from "react-icons/fa";
 
 export default function ChatPage() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
     <div className="chat-page">
-      <Sidebar />
+      {/* Toggle Button (mobile only) */}
+      <button
+        className={`sidebar-toggle-btn ${showSidebar ? "open" : ""}`}
+        onClick={() => setShowSidebar(!showSidebar)}
+      >
+        <FaBars />
+      </button>
+
+      {/* Sidebar */}
+      <div className={`sidebar-mobile ${showSidebar ? "open" : ""}`}>
+        <Sidebar />
+      </div>
+
+      {/* Main Chat */}
       <div className="chat-main">
         <ChatWindow />
         <ChatInput />
